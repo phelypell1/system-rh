07ef4c93442c0ca19d3905bbe8a580c647fd526b
phelypell1 <38192653+phelypell1@users.noreply.github.com>
2020-11-27 10:37:19 -0300

    commit back

---
+++ rh-system/src/test/java/br/com/controlesedex/RhSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controlesedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
